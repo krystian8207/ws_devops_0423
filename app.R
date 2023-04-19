4e4c9c1233398aee1cb2f1875e068c0d4f3958e9
Krystian Igras <krystian8207@gmail.com>
2023-04-19 19:11:49 +0200

    Initial application layout.

---
+++ app.R
@@ -0,0 +1,19 @@
+library(shiny)
+
+ui <- shinydashboardPlus::dashboardPage(
+  options = list(sidebarExpandOnHover = TRUE),
+  header = shinydashboardPlus::dashboardHeader(
+    title = "Sales App" |> shiny::h3(class = "dropdown", style = "display: inline;")
+  ),
+  sidebar = shinydashboardPlus::dashboardSidebar(
+    minified = TRUE, collapsed = FALSE
+  ),
+  body = shinydashboard::dashboardBody(
+  ),
+  title = "DashboardPage"
+)
+
+server <- function(input, output, session) {
+}
+
+shinyApp(ui, server, options = list(port = 8888, host = "0.0.0.0"))
